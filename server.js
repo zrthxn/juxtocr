c3f8c2ff6c2cf65e4780c818749416729bfb1ad9
Alisamar Husain <zrthxn@gmail.com>
2019-04-12 00:53:38 +0530

    PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const server = express()
 
-const PORT = 3000
+const PORT = 2019
 
 server.listen(PORT, ()=>{
     console.log("OCR View Sever Started ::", PORT)
